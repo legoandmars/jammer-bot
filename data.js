9300ba551826ff27f6b93be8a601fbfd47034caa
leogandmars <legoandmars@gmail.com>
2020-09-14 14:10:29 -0700

    removed data.json from upload

--- data.js
+++ data.js
@@ -1,12 +1,18 @@
 const fs = require('fs');
 
-const data = fs.readFileSync('./data/data.json');
-let parsedData = JSON.parse(data);
+let parsedData;
 
+if(fs.existsSync('./data/data.json')){
+    parsedData = JSON.parse(fs.readFileSync('./data/data.json'));
+}else{
+    parsedData = {"events":[]};
+    saveData();
+}
 function saveData(){
     const data = JSON.stringify(parsedData);
     fs.writeFileSync('./data/data.json', data);
 }
+
 function addEvent(user, song){
     parsedData.events.push({"user": user, "song": song, "time": Date.now()})
 }
